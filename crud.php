8ceb3af308f83cb17b677acbe8d7e7af6e4cdc24
Haniif Ahmad C <78332738+haniifacTI@users.noreply.github.com>
2022-05-23 19:53:55 +0700

    menambahkan file styleCrud.css dan pembuatan tabel crud

--- crud.php
+++ crud.php
@@ -1,8 +1,14 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['userLogin']) || $_SESSION['userLogin'] == ""){
-        header("Location: index.php");
-    }
+    require('functions.php');
+
+    // session_start();
+    // if(!isset($_SESSION['userLogin']) || $_SESSION['userLogin'] == ""){
+    //     header("Location: index.php");
+    // }
+
+    $sql = "SELECT * FROM olahraga";
+    $result = query($sql);
+    
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +20,42 @@
     <title>Sehat Dulu | Admin</title>
 </head>
 <body>
-    
+    <h2>
+        <a href="">+ Add Record</a>
+    </h2>
+
+    <table border="1px">
+        <thead>
+            <th>ID Olahraga</th>
+            <th>Nama</th>
+            <th>Durasi</th>
+            <th>Deskripsi</th>
+            <th>Video</th>
+            <th>ID Tipe</th>
+            <th>ID Kesulitan</th>
+            <th>Gambar</th>
+            <th>Langkah</th>
+            <th colspan="2">Action</th>
+        </thead>
+        <?php
+            for ($i=0; $i < count($result); $i++) {
+                echo "<tr>";
+                foreach ($result[$i] as $key => $value) {
+                    echo "<td>";
+                    echo $value;
+                    echo "</td>";
+                }
+
+                echo "<td>";
+                echo "<a href=''>Edit</a>";
+                echo "</td>";
+
+                echo "<td>";
+                echo "<a href=''>Delete</a>";
+                echo "</td>";
+            }
+            echo "</tr>";
+        ?>
+    </table>
 </body>
 </html>
